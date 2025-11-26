8fef0429987f9975105d2c0868def84eb54f12c4
candela <candepereyraa15@gmail.com>
2025-11-26 15:16:59 -0300

    cambios

--- hashUtils.js
+++ hashUtils.js
@@ -1,5 +1,13 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-export const generaHash = password => bcrypt.hashSync(password, 10);
+export const generaHash = (password) => bcrypt.hashSync(password, 10);
 export const validaPass = (pass, hash) => bcrypt.compareSync(pass, hash);
 
+export const generateToken = (user) => {
+  return jwt.sign(
+    { user: { _id: user._id, email: user.email, role: user.role } },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+};
--- utils/generateToken.js
+++ utils/generateToken.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const generateToken = (user) => {
   return jwt.sign(
-    { user },
+    { id: user._id, email: user.email, role: user.role },
     process.env.JWT_SECRET,
     { expiresIn: "1h" }
   );
